8861fa18affe5fc6dc1d23f59fab295ca3abec1c
Upenha <contato.upenha@gmail.com>
2023-01-17 20:47:28 -0300

    frontend dev

---
+++ web/src/components/HabitDay.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+interface HabitDayProps {
+  // completed: number
+}
+
+
+const HabitDay = (props: HabitDayProps) => {
+  return (
+    <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+  )
+};
+
+export default HabitDay;
